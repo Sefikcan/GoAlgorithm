dd9029d086a576ad3d0343b0afa6e174de3e1f40
Şefik Can Kanber <sefikcankanber@192.168.1.50>
2023-11-22 10:57:10 +0300

    BubbleSort algorithm added

--- SortingAlgorithm/BubbleSort.go
+++ SortingAlgorithm/BubbleSort.go
@@ -1,1 +1,37 @@
 package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{5, 2, 7, 9, 1, 0, 6, 3, 4}
+	names := []string{"Ali", "Veli", "Ayşe", "Fatma"}
+
+	bubbleSort(numbers)
+	bubbleSort(names)
+
+	fmt.Println(numbers)
+	fmt.Println(names)
+}
+
+// this algorithm compare one by one and swap items
+func bubbleSort(arr interface{}) {
+	switch array := arr.(type) {
+	case []int:
+		for i := 0; i < len(array); i++ {
+			for j := 0; j < len(array)-i-1; j++ {
+				if array[j] > array[j+1] {
+					// swap elements
+					array[j], array[j+1] = array[j+1], array[j]
+				}
+			}
+		}
+	case []string:
+		for i := 0; i < len(array); i++ {
+			for j := 0; j < len(array)-i-1; j++ {
+				if array[j] > array[j+1] {
+					array[j], array[j+1] = array[j+1], array[j]
+				}
+			}
+		}
+	}
+}
